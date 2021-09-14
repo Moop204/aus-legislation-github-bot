7f7f4464a0055d6de0d565aa884e214750bef408
Moop204 <andrew.nguyen2045@outlook.com>
2021-09-14 22:03:31 +1000

    Restructured project

--- index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var octokit_1 = require("octokit");
-var octokit = new octokit_1.Octokit({ auth: process.env.PAT });
-var login = (await octokit.rest.users.getAuthenticated()).data.login;
-console.log("Login %s", login);
