5af614b9bfbd8d22f714cd9978d09b13c5d1334e
Moop204 <andrew.nguyen2045@outlook.com>
2021-09-14 22:00:09 +1000

    Added sample commit function

--- index.ts
+++ index.ts
@@ -4,20 +4,54 @@ const octokit = new Octokit({ auth: process.env.PAT });
 
 console.log(process.env.PAT);
 
-const testOcto = async () => {
+const testOctoCommit = async () => {
+  const owner = "Moop204";
+  const repo = "aus-legislation";
+  const branch = "heads/master";
+  // Get existing commits
   const commits = await octokit.rest.repos.listCommits({
-    owner: "Moop204",
-    repo: "aus-legislation.git",
+    owner,
+    repo,
   });
 
-  console.log(commits);
-  // octokit.rest.git.createCommit({
-  //   owner: "Moop204",
-  //   repo: "https://github.com/Moop204/aus-legislation",
-  //   message: "testing octokit",
-  //   tree: "idk",
-  //   author: { name: "Moop204", email: "z5162792@unsw.edu.au" },
-  // });
-};
+  // Take latest one
+  const latestCommit = commits.data[0];
 
-testOcto();
+  // Make tree
+  const files = [
+    {
+      mode: "100644" as const,
+      path: "src/test/a.txt",
+      content: "its only nine",
+    },
+  ];
+
+  const {
+    data: { sha: treeSHA },
+  } = await octokit.rest.git.createTree({
+    owner,
+    repo,
+    tree: files,
+    base_tree: latestCommit.sha,
+  });
+  const {
+    data: { sha: newCommitSHA },
+  } = await octokit.rest.git.createCommit({
+    owner: "Moop204",
+    repo,
+    message: "testing octokit",
+    tree: treeSHA,
+    author: {
+      name: process.env.NAME as string,
+      email: process.env.EMAIL as string,
+    },
+    parents: [latestCommit.sha],
+  });
+  const response = await octokit.rest.git.updateRef({
+    owner,
+    repo,
+    ref: branch,
+    sha: newCommitSHA,
+  });
+  console.log(response);
+};
